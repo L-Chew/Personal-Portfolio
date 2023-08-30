b15af9e8dd3afe180bf0ffe9297a0a1c5a10c121
Lorene Chew <lorene.chew@gmail.com>
2023-08-30 00:39:29 -0700

    About: created component; App: added About to render

---
+++ client/src/Components/About.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const About = () => {
+
+  return (
+    <>
+      <div className='text-center text-3xl'>About</div>
+    </>
+  )
+};
+
+export default About;
--- client/src/Components/App.jsx
+++ client/src/Components/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Hero from './Hero.jsx';
+import About from './About.jsx';
 
 const App = () => {
   return (
     <>
       <Hero />
+      <About />
     </>
   )
 }
