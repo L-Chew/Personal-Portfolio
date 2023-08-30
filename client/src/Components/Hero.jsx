b1e7fc05894e77f4a09a960cd407d337c1476b4c
Lorene Chew <lorene.chew@gmail.com>
2023-08-29 22:01:08 -0700

    Hero: updated grid layout and added blue accent color

--- client/src/Components/Hero.jsx
+++ client/src/Components/Hero.jsx
@@ -2,18 +2,23 @@ import React from 'react';
 
 const Hero = () => {
   return (
-    <div className='w-screen'>
-      <div className='grid grid-rows-8 grid-cols-12 gap-1 h-screen content-center'>
-        <div className='row-start-2 col-span-5 col-start-2 text-[50px]'>
-          Hello, I'm <span className='text-[#5EEAD4]'>Lorene Chew</span>.
+    <div className='w-screen min-h-screen'>
+      <div className='grid grid-cols-12 gap-1 grid-rows-6'>
+        <div className='col-start-2 col-span-6 row-start-3'>
+          <div className='text-[50px]'>
+            Hello, I'm <span className='text-[#5EEAD4]'>Lorene Chew</span>.
+          </div>
+          <div className='text-[35px]'>
+            I am a full-stack web developer.
+          </div>
+          <br></br>
+          <button className='rounded bg-[#3B4560]'>
+            {`Learn More ->`}
+          </button>
         </div>
-        <div className='row-start-3 row-span-2 col-span-5 col-start-2 text-[35px]'>
-          I am a full-stack web developer.
+        <div className='col-end-13 col-span-4 row-span-6'>
+          <div className='h-[100vh] bg-[#3B4560] bg-opacity-25'></div>
         </div>
-        <button className='row-start-6 col-span-2 col-start-2 rounded bg-[#3B4560]'>
-          {`Learn More ->`}
-        </button>
-        <div className='bg-[#5EEAD4] row-span-8 col-end-13 col-span-5'>box</div>
       </div>
     </div>
   )
