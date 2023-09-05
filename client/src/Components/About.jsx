27d9599aa8013c1a5129b18407cae4e3aca27594
Lorene Chew <lorene.chew@gmail.com>
2023-09-05 16:35:21 -0700

    About: updated format; included placeholder text

--- client/src/Components/About.jsx
+++ client/src/Components/About.jsx
@@ -4,7 +4,11 @@ const About = () => {
 
   return (
     <>
-      <div className='text-center text-3xl'>About</div>
+      <div className='grid grid-cols-8'>
+        <div className='col-start-1 col-span-9 text-2xl'>
+          In late 2021, I've decided to quit my job to pursue a new career in software development. It all started when I was leading a project with a goal of consolidate all cross-functional teams data to one local source.
+        </div>
+      </div>
     </>
   )
 };
