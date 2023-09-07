19b0768c5115303e3f579d14b7848eefc0570d0f
Lorene Chew <lorene.chew@gmail.com>
2023-09-06 21:41:28 -0700

    Hero: updated colors

--- client/src/Components/Hero.jsx
+++ client/src/Components/Hero.jsx
@@ -1,32 +1,36 @@
 import React from 'react';
 
 const Hero = () => {
+  const handleLearnMore = () => {
+    alert('clicked')
+  }
+
   return (
     <div className='w-screen min-h-screen'>
       <div className='grid grid-cols-12 gap-1 grid-rows-5'>
         <div className='col-start-2 col-span-6 row-start-3 space-y-4'>
           <div className='text-6xl'>
-            Hello, I'm <span className='text-[#5EEAD4]'>Lorene Chew</span>.
+            Hello, I'm <span className='text-[#2DD4BE]'>Lorene Chew</span>.
           </div>
           <div className='text-4xl'>
             I am a full-stack web developer.
           </div>
-          <button className='rounded bg-[#3B4560] h-10 w-32'>
+          <button onClick={handleLearnMore} className='rounded bg-[#495678] h-10 w-32'>
             {`Learn More ->`}
           </button>
         </div>
         <div className='row-span-6 col-end-13 col-span-5'>
           <div className='grid grid-cols-5 grid-rows-4'>
             <div className='col-start-1 col-span-3 row-start-2 row-span-2 z-10'>
-              <div className='h-[45vh] bg-[#5EEAD4] bg-opacity-90'></div>
+              <div className='h-[45vh] bg-[#2DD4BE] bg-opacity-90'></div>
             </div>
             <div className='col-start-3 col-span-3 row-start-1 row-span-4'>
-              <div className='h-[100vh] bg-[#3B4560] bg-opacity-25'></div>
+              <div className='h-[100vh] bg-[#495678] bg-opacity-25'></div>
             </div>
           </div>
         </div>
       </div>
-      <div className='h-1 bg-[#5EEAD4] mb-10 mx-20'></div>
+      <div className='h-1 bg-[#2DD4BE] mb-10 mx-20'></div>
     </div>
   )
 }
