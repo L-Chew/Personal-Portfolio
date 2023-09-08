6090d9257559bbb614f246199374c56d9657768d
Lorene Chew <lorene.chew@gmail.com>
2023-09-08 13:58:14 -0700

    About: updated wording

--- client/src/Components/About.jsx
+++ client/src/Components/About.jsx
@@ -6,7 +6,7 @@ const About = () => {
     <>
       <div className='grid grid-cols-8'>
         <div className='col-start-1 col-span-9 text-2xl'>
-          In late 2021, I've decided to quit my job to pursue a new career in software development. It all started from when I was leading a project with a goal of consolidating all cross-functional teams' data to one local source.
+          In late 2021, I decided to quit my job to pursue a new career in software development. It all started from when I was leading a project with a goal of consolidating all cross-functional teams' data, originally in spreadsheets, to one local source.
         </div>
       </div>
     </>
