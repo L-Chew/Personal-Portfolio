995c91439a5197e008bbe5f365f9a2ac506d1a61
Lorene Chew <lorene.chew@gmail.com>
2023-09-08 16:30:38 -0700

    About, Experience: added ID for scroll reference

--- client/src/Components/Experience.jsx
+++ client/src/Components/Experience.jsx
@@ -41,7 +41,7 @@ const Experience = () => {
   ]
 
   return (
-    <div>
+    <div id='experience'>
       <div>
         <h2 className='flex my-12 text-2xl text-[#2DD4BE] font-semibold uppercase'>Experience</h2>
       </div>
