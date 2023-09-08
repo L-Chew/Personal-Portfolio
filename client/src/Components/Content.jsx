b74c92aff53f81c82c0eeb0fc8248b686a8c96a2
Lorene Chew <lorene.chew@gmail.com>
2023-09-08 16:29:47 -0700

    Content: Added scroll feature

--- client/src/Components/Content.jsx
+++ client/src/Components/Content.jsx
@@ -1,21 +1,32 @@
-import React from 'react';
+import React, { useRef } from 'react';
+import { NavLink } from 'react-router-dom';
 import About from './About.jsx';
 import Experience from './Experience.jsx';
+import Projects from './Projects.jsx';
 
 const Content = () => {
+  const myScrollRef = useRef(null);
+
+  const handleClick = (e) => {
+    document.getElementById(`${e.target.value}`).scrollIntoView({block: 'end', behavior: 'smooth'});
+  }
+
+
   return (
     <div className='ml-20 mr-20'>
-      <div className='grid grid-cols-2'>
-        <div className='col-start-1'>
-          <div className='flex flex-col text-xl'>
-            <button>About</button>
-            <button>Experience</button>
-            <button>Projects</button>
+      <NavLink to='/' className='fixed w-screen mt-10 text-3xl text-[#2DD4BE]'> LC </NavLink>
+      <div className='grid grid-cols-8'>
+        <div className='col-start-2 col-span-2'>
+          <div className='fixed flex flex-col h-screen justify-center pl-20 text-xl'>
+            <button onClick={handleClick} value='about'> About </button>
+            <button onClick={handleClick} value='experience'> Experience </button>
+            <button onClick={handleClick} value='projects'> Projects </button>
           </div>
         </div>
-        <div className='col-start-2'>
+        <div className='col-start-5 col-span-4 my-20 h-100'>
           <About />
           <Experience />
+          <Projects />
         </div>
       </div>
     </div>
