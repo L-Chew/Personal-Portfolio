51e9d027c4615dc634ad13b7cd8e19e0f6a24f69
Lorene Chew <lorene.chew@gmail.com>
2023-09-08 16:30:12 -0700

    Projects: created new component

---
+++ client/src/Components/Projects.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Projects = () => {
+  return (
+    <div id='projects'>
+      <h2 className='flex my-12 text-2xl text-[#2DD4BE] font-semibold uppercase'>Projects</h2>
+      <div className='flex'>
+        placeholder
+      </div>
+    </div>
+  )
+}
+
+export default Projects;
