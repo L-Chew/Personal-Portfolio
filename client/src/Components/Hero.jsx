b93ad32d5dd8212ea1edf9598f501f377d56649a
Lorene Chew <lorene.chew@gmail.com>
2023-10-30 14:54:05 -0700

    Hero: added profile pic

--- client/src/Components/Hero.jsx
+++ client/src/Components/Hero.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import profilePic from '../Images/profilePic.jpeg';
 
 const Hero = () => {
 
@@ -19,8 +20,13 @@ const Hero = () => {
         </div>
         <div className='row-span-6 col-end-13 col-span-5'>
           <div className='grid grid-cols-5 grid-rows-4'>
-            <div className='col-start-1 col-span-3 row-start-2 row-span-2 z-10'>
-              <div className='h-[45vh] bg-[#2DD4BE] bg-opacity-90'></div>
+            <div className='col-start-1 col-span-3 row-start-2 row-span-2 z-10 relative'>
+              <div className='h-[45vh]'>
+                {/* Display the profile pic */}
+                <img src={profilePic} alt='profile pic' className="object-cover w-full h-full" />
+                {/* Overlay the background color on top of the profilePic */}
+                <div className="absolute inset-0 h-[45vh] bg-[#2DD4BE] bg-opacity-25"></div>
+              </div>
             </div>
             <div className='col-start-3 col-span-3 row-start-1 row-span-4'>
               <div className='h-[100vh] bg-[#495678] bg-opacity-25'></div>
