e18e89ab2ad850a08dbda194fa57c6677d56ce5e
Lorene Chew <lorene.chew@gmail.com>
2023-10-30 17:17:02 -0700

    GetInTouch: create new component

---
+++ client/src/Components/GetInTouch.jsx
@@ -0,0 +1,14 @@
+import react from 'react';
+
+const GetInTouch = () => {
+  return (
+    <>
+      <form>
+        <input type='text' name='name'/>
+        <input type='submit' value='Submit'/>
+      </form>
+    </>
+  )
+}
+
+export default GetInTouch;
