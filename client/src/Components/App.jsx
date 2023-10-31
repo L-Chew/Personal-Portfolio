e90fee089f0166c3b83d3acb453c528dbc1edce2
Lorene Chew <lorene.chew@gmail.com>
2023-10-30 17:09:13 -0700

    App: moved background color and text color to app from html to prevent inline script

--- client/src/Components/App.jsx
+++ client/src/Components/App.jsx
@@ -2,13 +2,17 @@ import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import Hero from './Hero.jsx';
 import Content from './Content.jsx';
+import GetInTouch from './GetInTouch.jsx';
 
 const App = () => {
   return (
-    <Routes>
-      <Route exact path="/" element={<Hero />} />
-      <Route path="/learn-more" element={<Content />} />
-    </Routes>
+    <div className='bg-[#0f172a] text-[#CBD5E1]'>
+      <Routes>
+        <Route exact path="/" element={<Hero />} />
+        <Route path="/learn-more" element={<Content />} />
+      </Routes>
+      <GetInTouch />
+    </div>
   )
 }
 
