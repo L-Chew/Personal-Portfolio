73a04d4a78718bbf9fb102efafabb20c3d628ecf
Lorene Chew <lorene.chew@gmail.com>
2023-11-02 13:56:29 -0700

    App, styles.css: set body background color and text color is external css stylesheet; removed bg and color settings from App component

--- client/src/Components/App.jsx
+++ client/src/Components/App.jsx
@@ -6,13 +6,13 @@ import GetInTouch from './GetInTouch.jsx';
 
 const App = () => {
   return (
-    <div className='bg-[#0f172a] text-[#CBD5E1]'>
+    <>
       <Routes>
         <Route exact path="/" element={<Hero />} />
         <Route path="/learn-more" element={<Content />} />
       </Routes>
-      <GetInTouch />
-    </div>
+      {/* <GetInTouch /> */}
+    </>
   )
 }
 
