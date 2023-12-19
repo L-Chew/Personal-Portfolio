d05d5966b66327fabc264fe31ea4a45596fad578
Lorene Chew <lorene.chew@gmail.com>
2023-12-19 14:54:23 -0500

    Preloader: deleted; Typewriter: renamed as preloader and modified className

--- client/src/Components/Preloader.jsx
+++ client/src/Components/Preloader.jsx
@@ -1,16 +1,25 @@
-import React, { useEffect } from 'react';
-import TypeWriter from './TypeWriter.jsx';
+import React from 'react';
+import { useTypewriter, Cursor } from 'react-simple-typewriter';
 
 const Preloader = () => {
-
-  useEffect(() => {
-
-  }, [])
-
+  const [text] = useTypewriter({
+    words: [`Hello, I'm Lorene.`, `Welcome to my world!`],
+    typeSpeed: 120,
+    deleteSpeed: 120
+  })
   return (
-    <div className='preloader h-[100vh] w-screen fixed flex items-center justify-center mx-0 mb-0 z-55'>
-      <TypeWriter />
+  <div className='preloader h-[100vh] w-screen fixed flex items-center justify-center mx-0 mb-0 z-55'>
+    <div className='textContainer m-50px'>
+      <div>
+        <span>
+          {text}
+        </span>
+        <span>
+          <Cursor cursorStyle='|' />
+        </span>
+      </div>
     </div>
+  </div>
   )
 }
 
