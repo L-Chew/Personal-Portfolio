ad71c27f8f5a54aaf15c451b5867540d98e35846
Lorene Chew <lorene.chew@gmail.com>
2023-12-19 17:36:07 -0500

    App: added Preloader component - load on initial page load; comment out for Preloader component developement

--- client/src/Components/App.jsx
+++ client/src/Components/App.jsx
@@ -1,17 +1,25 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Route, Routes } from 'react-router-dom';
+import Preloader from './Preloader.jsx';
 import Hero from './Hero.jsx';
 import Content from './Content.jsx';
 import GetInTouch from './GetInTouch.jsx';
 
 const App = () => {
+  // useEffect(() => {
+  //   setTimeout(() => setLoading(false), 10000)
+  // , []})
+
   return (
     <>
-      <Routes>
-        <Route exact path="/" element={<Hero />} />
-        <Route path="/learn-more" element={<Content />} />
-      </Routes>
-      {/* <GetInTouch /> */}
+      {
+        loading ? <Preloader />
+        :
+        <Routes>
+          <Route exact path="/" element={<Hero />} />
+          <Route path="/learn-more" element={<Content />} />
+        </Routes>
+      }
     </>
   )
 }
