3f313d2c5abc79d78bbf010abdee60fbaef927af
Lorene Chew <lorene.chew@gmail.com>
2023-12-21 13:21:26 -0500

    App: removed hero and getInTouch components

--- client/src/Components/App.jsx
+++ client/src/Components/App.jsx
@@ -1,15 +1,13 @@
 import React, { useEffect, useState } from 'react';
 import { Route, Routes } from 'react-router-dom';
 import Preloader from './Preloader.jsx';
-import Hero from './Hero.jsx';
 import Content from './Content.jsx';
-import GetInTouch from './GetInTouch.jsx';
 
 const App = () => {
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    setTimeout(() => setLoading(false), 10000)
+    setTimeout(() => setLoading(false), 6500)
   , []})
 
   return (
@@ -18,7 +16,7 @@ const App = () => {
         loading ? <Preloader />
         :
         <Routes>
-          <Route exact path="/" element={<Hero />} />
+          {/* <Route exact path="/" element={<Hero />} /> */}
           <Route path="/learn-more" element={<Content />} />
         </Routes>
       }
