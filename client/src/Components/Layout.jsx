397c47512972d534ae16ba95420eb7d0701e5e20
Lorene Chew <lorene.chew@gmail.com>
2024-01-11 17:45:59 -0800

    Layout: added react router layout to have navigation appear on all pages

---
+++ client/src/Components/Layout.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Outlet } from 'react-router-dom';
+import NavBar from './NavBar.jsx';
+
+const Layout = () => {
+  return (
+    <div className='animate-fadeIn flex flex-col mx-auto w-full max-w-screen-sm px-8 md:max-w-screen-md lg:max-w-screen-lg xl:max-w-screen-2xl'>
+      <header>
+        <NavBar />
+      </header>
+
+      <main>
+        <Outlet />
+      </main>
+    </div>
+  )
+}
+
+export default Layout;
