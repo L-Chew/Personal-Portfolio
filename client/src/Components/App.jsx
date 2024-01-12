ee69a4aef279ddc279201868de88310182db01d7
Lorene Chew <lorene.chew@gmail.com>
2024-01-12 15:29:15 -0800

    App: adding new routes placeholders

--- client/src/Components/App.jsx
+++ client/src/Components/App.jsx
@@ -4,8 +4,11 @@ import Preloader from './Preloader.jsx';
 // import Content from './Content.jsx';
 // import Content2 from './Content2.jsx';
 import Projects from './Projects.jsx';
+import Reading from './Reading.jsx';
+import Hobbies from './Hobbies.jsx';
 import GetInTouch from './GetInTouch.jsx';
 import Layout from './Layout.jsx';
+import About from './About.jsx';
 
 const App = () => {
   const [loading, setLoading] = useState(false);
@@ -21,7 +24,10 @@ const App = () => {
         :
         <Routes>
           <Route exact path='/' element={<Layout />}>
+            <Route path='/' element={<About />} />
             <Route path='/projects' element={<Projects />} />
+            <Route path='/reading' element={<Reading />} />
+            <Route path='/hobbies' element={<Hobbies />} />
             <Route path='/getInTouch' element={<GetInTouch />} />
           </Route>
         </Routes>
