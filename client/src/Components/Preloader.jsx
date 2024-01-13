3a6ac1394180a2d23dc5efb16176e7f90bfcfe50
Lorene Chew <lorene.chew@gmail.com>
2024-01-12 22:35:25 -0800

    Preloader: updated cosmetics

--- client/src/Components/Preloader.jsx
+++ client/src/Components/Preloader.jsx
@@ -4,27 +4,28 @@ import { TypeAnimation } from 'react-type-animation';
 const Preloader = () => {
   const [textColor, setTextColor] = useState('white');
 
-  const [italic, setItalic] = useState(false);
+  const [italicize, setItalicize] = useState(false);
 
   const intro = `I'm Lorene`;
 
   return (
-    <div className='preloader h-[100vh] w-screen fixed flex items-center justify-center m-50px z-55 text-3xl font-bold pointer-events-none' style={{ color:textColor }}>
-      <TypeAnimation 
+    <div className='preloader h-[100vh] w-screen fixed flex items-center justify-center m-50px z-55 text-5xl font-extralight pointer-events-none' style={{ color:textColor }}>
+      <TypeAnimation
       sequence={[
         `Hello`,
         1000,
-        () => setTextColor('pink'),
+        () => setTextColor('#fac1e9'),
         `I'm Lorene`,
         1000,
-        () => setTextColor('white'),
-        'Welcome to my world!',
+        () => setTextColor('#CBD5E1'),
+        'Welcome to my bubble!',
       ]}
       wrapper="span"
       cursor={false}
       repeat={0}
       style={{
-        whiteSpace: 'pre-line'
+        whiteSpace: 'pre-line',
+        'font-family': 'Fraunces'
       }}
       />
     </div>
