c862b0729a18c3b1608529422c5bfa108ec7ac83
Lorene Chew <lorene.chew@gmail.com>
2024-01-12 21:27:48 -0800

    Hobbies, Reading: added header

--- client/src/Components/Hobbies.jsx
+++ client/src/Components/Hobbies.jsx
@@ -3,6 +3,13 @@ import React from 'react';
 const Hobbies = () => {
   return (
     <>
+      <div id='projects' className='animate-fadeIn h-[100vh]'>
+        <div className='flex flex-col gap-4 md:gap-8 px-2'>
+          <h2 className='sm:text-xl lg:text-2xl xl:text-8xl text-[#fac1e9] font-Fraunces font-extralight'>
+            hobbies.
+          </h2>
+        </div>
+      </div>
     </>
   )
 }
--- client/src/Components/Reading.jsx
+++ client/src/Components/Reading.jsx
@@ -4,6 +4,13 @@ import { Link } from 'react-router-dom';
 const Readings = () => {
   return (
     <>
+      <div id='projects' className='animate-fadeIn h-[100vh]'>
+        <div className='flex flex-col gap-4 md:gap-8 px-2'>
+          <h2 className='sm:text-xl lg:text-2xl xl:text-8xl text-[#fac1e9] font-Fraunces font-extralight'>
+            reading.
+          </h2>
+        </div>
+      </div>
     </>
   )
 }
