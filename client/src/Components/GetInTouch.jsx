17c1e5d73d21a281d294e3d9b5d2e473d2478b1d
Lorene Chew <lorene.chew@gmail.com>
2024-01-16 16:50:35 -0800

    GetInTouch: added title

--- client/src/Components/GetInTouch.jsx
+++ client/src/Components/GetInTouch.jsx
@@ -2,12 +2,17 @@ import react from 'react';
 
 const GetInTouch = () => {
   return (
-    <>
-      <form>
-        <input type='text' name='name'/>
-        <input type='submit' value='Submit'/>
-      </form>
-    </>
+    <div className='animate-fadeIn'>
+      <div className='flex flex-col gap-4 md:gap-8 px-2 pb-20'>
+          <h2 className='sm:text-xl lg:text-2xl xl:text-8xl text-[#fac1e9] font-Fraunces font-extralight'>
+            get in touch.
+          </h2>
+          <form>
+            <input type='text' name='name'/>
+            <input type='submit' value='Submit'/>
+          </form>
+        </div>
+    </div>
   )
 }
 
