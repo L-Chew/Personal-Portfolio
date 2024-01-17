7496042737d64d2e3f4001b51e803218fea9a1ea
Lorene Chew <lorene.chew@gmail.com>
2024-01-16 16:50:14 -0800

    NavBar: added hover effects on navigation

--- client/src/Components/NavBar.jsx
+++ client/src/Components/NavBar.jsx
@@ -22,7 +22,7 @@ const NavBar = () => {
       'link': '/hobbies',
       'where': 'Hobbies'
     }
-  ]
+  ];
 
   const buttons = [
     {
@@ -41,41 +41,44 @@ const NavBar = () => {
       'src': info,
       'alt': 'Contact'
     }
-  ]
+  ];
 
   return (
     <>
       <nav className='sticky top-0 flex justify-between items-center text-sm py-4 px-1'>
         {/* right text nav */}
         <ul className='flex rounded-lg'>
-          {navs.map(nav => (
-            <li className='py-1 px-2 hover:bg-[#495678] hover:bg-opacity-50 hover:rounded-md'>
-              <Link to={nav.link}> {nav.where} </Link>
-            </li>
+          {navs.map((nav, index) => (
+            <div key={index} className='relative group'>
+              <div className='absolute -inset-0.5 bg-gradient-to-r from-pink-600 to-purple-600 rounded-md blur opacity-0 group-hover:opacity-50 transition duration-1000 group-hover:duration-200'></div>
+              <li className='relative py-1 px-2 hover:rounded-md transition duration-200 group-hover:scale-110'>
+                <Link to={nav.link}> {nav.where} </Link>
+              </li>
+            </div>
           ))}
         </ul>
         {/* left icon nav */}
         <ul className='flex rounded-lg'>
-          {buttons.map(button => (
-            (button.href
-            ?
-            <li className='py-1 px-2 hover:bg-[#495678] hover:bg-opacity-50 hover:rounded-md'>
-              <a href={button.href} target='_blank' rel='noopener noreferrer'>
-                <img className='h-9' src={button.src} alt={button.alt}/>
-              </a>
-            </li>
-            :
-            <li className='py-1 px-2 hover:bg-[#495678] hover:bg-opacity-50 hover:rounded-md'>
-              <Link to={button.link}>
-                <img className='h-9' src={button.src} alt={button.alt}/>
-              </Link>
-            </li>
-            )
+          {buttons.map((button, index) => (
+            <div key={index} className='relative group'>
+              <div className='absolute -inset-0.5 bg-gradient-to-r from-pink-600 to-purple-600 rounded-md blur opacity-0 group-hover:opacity-50 transition duration-1000 group-hover:duration-200'></div>
+              <li key={index} className='py-1 px-2'>
+                {button.href ? (
+                  <a href={button.href} target='_blank' rel='noopener noreferrer'>
+                    <img className='h-9 transition duration-300 transform group-hover:scale-110' src={button.src} alt={button.alt}/>
+                  </a>
+                ) : (
+                  <Link to={button.link}>
+                    <img className='h-9 transition duration-300 transform group-hover:scale-110' src={button.src} alt={button.alt}/>
+                  </Link>
+                )}
+              </li>
+            </div>
           ))}
         </ul>
       </nav>
     </>
-  )
-}
+  );
+};
 
 export default NavBar;
