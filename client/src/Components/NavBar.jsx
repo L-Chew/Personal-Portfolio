ce1be8819f324b3a35933917ae4baa9b089ec776
Lorene Chew <lorene.chew@gmail.com>
2024-01-18 18:48:30 -0800

    NavBar: removed sticky top to Layout component - react router wrapping did not allow for sticky top

--- client/src/Components/NavBar.jsx
+++ client/src/Components/NavBar.jsx
@@ -44,40 +44,38 @@ const NavBar = () => {
   ];
 
   return (
-    <>
-      <nav className='sticky top-0 flex justify-between items-center text-sm py-4 px-1'>
-        {/* right text nav */}
-        <ul className='flex rounded-lg'>
-          {navs.map((nav, index) => (
-            <div key={index} className='relative group'>
-              <div className='absolute -inset-0.5 bg-gradient-to-r from-pink-600 to-purple-600 rounded-md blur opacity-0 group-hover:opacity-50 transition duration-1000 group-hover:duration-200'></div>
-              <li className='relative py-1 px-2 hover:rounded-md transition duration-200 group-hover:scale-110'>
-                <Link to={nav.link}> {nav.where} </Link>
-              </li>
-            </div>
-          ))}
-        </ul>
-        {/* left icon nav */}
-        <ul className='flex rounded-lg'>
-          {buttons.map((button, index) => (
-            <div key={index} className='relative group'>
-              <div className='absolute -inset-0.5 bg-gradient-to-r from-pink-600 to-purple-600 rounded-md blur opacity-0 group-hover:opacity-50 transition duration-1000 group-hover:duration-200'></div>
-              <li key={index} className='py-1 px-2'>
-                {button.href ? (
-                  <a href={button.href} target='_blank' rel='noopener noreferrer'>
-                    <img className='h-9 transition duration-300 transform group-hover:scale-110' src={button.src} alt={button.alt}/>
-                  </a>
-                ) : (
-                  <Link to={button.link}>
-                    <img className='h-9 transition duration-300 transform group-hover:scale-110' src={button.src} alt={button.alt}/>
-                  </Link>
-                )}
-              </li>
-            </div>
-          ))}
-        </ul>
-      </nav>
-    </>
+    <nav className='flex justify-between items-center text-sm py-4 px-1'>
+      {/* right text nav */}
+      <ul className='flex rounded-lg'>
+        {navs.map((nav, index) => (
+          <div key={index} className='relative group'>
+            <div className='absolute -inset-0.5 bg-gradient-to-r from-pink-600 to-purple-600 rounded-md blur opacity-0 group-hover:opacity-50 transition duration-1000 group-hover:duration-200'></div>
+            <li className='relative py-1 px-2 hover:rounded-md transition duration-200 group-hover:scale-110'>
+              <Link to={nav.link}> {nav.where} </Link>
+            </li>
+          </div>
+        ))}
+      </ul>
+      {/* left icon nav */}
+      <ul className='flex rounded-lg'>
+        {buttons.map((button, index) => (
+          <div key={index} className='relative group'>
+            <div className='absolute -inset-0.5 bg-gradient-to-r from-pink-600 to-purple-600 rounded-md blur opacity-0 group-hover:opacity-50 transition duration-1000 group-hover:duration-200'></div>
+            <li key={index} className='py-1 px-2'>
+              {button.href ? (
+                <a href={button.href} target='_blank' rel='noopener noreferrer'>
+                  <img className='h-9 transition duration-300 transform group-hover:scale-110' src={button.src} alt={button.alt}/>
+                </a>
+              ) : (
+                <Link to={button.link}>
+                  <img className='h-9 transition duration-300 transform group-hover:scale-110' src={button.src} alt={button.alt}/>
+                </Link>
+              )}
+            </li>
+          </div>
+        ))}
+      </ul>
+    </nav>
   );
 };
 
