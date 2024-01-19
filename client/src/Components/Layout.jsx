5a935bbfcece4a8d99ad9e7af0cc21b12de83689
Lorene Chew <lorene.chew@gmail.com>
2024-01-18 18:47:34 -0800

    Layout: added nav sticky top

--- client/src/Components/Layout.jsx
+++ client/src/Components/Layout.jsx
@@ -5,7 +5,7 @@ import NavBar from './NavBar.jsx';
 const Layout = () => {
   return (
     <div className='animate-fadeIn flex flex-col mx-auto w-full max-w-screen-sm px-8 md:max-w-screen-md lg:max-w-screen-lg xl:max-w-screen-2xl'>
-      <header>
+      <header className='sticky top-0 z-10'>
         <NavBar />
       </header>
 
