1b16508dbc817ef9e9c005f2ca453c3937e1f92c
Lorene Chew <lorene.chew@gmail.com>
2024-01-19 11:06:49 -0800

    ReadingCards: fine tuned layout details

--- client/src/Components/ReadingCards.jsx
+++ client/src/Components/ReadingCards.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 
 
 const ReadingCards = () => {
@@ -7,13 +7,12 @@ const ReadingCards = () => {
       <img className='w-full h-full object-cover transition-transform duration-500  group-hover:scale-110' src='https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1662607049i/61434791.jpg' alt='book' />
       <div className='absolute inset-0 bg-gradient-to-b from-transparent via-transparent to-black group-hover:from-black/70 group-hover:via-black/60 group-hover:to-black/70'>
       </div>
-      <div className='absolute inset-0 flex translate-y-[60%] flex-col items-center justify-center px-9 text-center transition-all duration-500 group-hover:translate-y-0 pb-[100px]'>
-        <h2 className='text-3xl font-bold text-white'>Book Title</h2>
-        <p className='mb-3 text-lg italic text-white opacity-0 transition-opacity duration-300 group-hover:opacity-100'>Reading Status</p>
-        <p className='text-lg italic text-white'>Author</p>
+      <div className='absolute inset-0 flex opacity-0 translate-y-[60%] flex-col items-center justify-center px-9 text-center transition-all duration-500 group-hover:opacity-100 group-hover:translate-y-0'>
+        <h2 className='text-2xl font-bold text-white'>Heaven Officials' Blessing Vol 1</h2>
+        <p className='mb-3 text-lg italic text-white opacity-0 transition-opacity duration-300 group-hover:opacity-100'>Mo Xiang Tong Xiu</p>
+        <div className='text-sm italic text-white bg-green-900 rounded-2xl px-2 py-1 opacity-0 transition-opacity duration-300 group-hover:opacity-100'>Read</div>
       </div>
     </div>
-
   )
 }
 
