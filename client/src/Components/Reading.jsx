ee05d0d17a907433c7921f8a5ec0559d75a5be80
Lorene Chew <lorene.chew@gmail.com>
2024-01-19 21:46:37 -0800

    Reading: fetched csv data

--- client/src/Components/Reading.jsx
+++ client/src/Components/Reading.jsx
@@ -1,7 +1,37 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import Papa from 'papaparse';
 import ReadingCards from './ReadingCards.jsx';
 
 const Readings = () => {
+
+  const [csvData, setCsvData] = useState([]);
+  const [books, setBooks] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+       // fetch URL is relative to the current HTML page, not the JavaScript file
+        const response = await fetch('./goodreads.csv');
+        // csv not a json therefore needs to use text
+        const csvText = await response.text();
+
+        Papa.parse(csvText, {
+          header: true,
+          skipEmptyLines: true,
+          complete: (results=> {
+            setCsvData(results.data);
+            console.log('results data: ', results.data)
+            setBooks(Object.values(results.data));
+
+          })
+        });
+      } catch (error) {
+        console.error('Error parsing CSV:', error);
+      }
+    };
+    fetchData();
+  }, []);
+
   return (
     <div id='reading' className='animate-fadeIn h-[100vh]'>
       <div className='flex flex-col gap-4 md:gap-8 px-2 pb-20'>
@@ -11,7 +41,10 @@ const Readings = () => {
       </div>
       {/*Cards*/}
       <div className='grid grid-cols-1 gap-5 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 grid-auto-rows-auto grid-auto-flow-dense'>
-        <ReadingCards />
+        {books.map(book => (book.Display === 'yes' ?
+          <ReadingCards key={book.Title} book={book}/>
+          : ''
+        ))}
       </div>
     </div>
   )
