913a1b859a91e489125f7cc805e1c599c60d5eed
Lorene Chew <lorene.chew@gmail.com>
2024-01-22 12:04:24 -0800

    NavBar: cleaned up variables names

--- client/src/Components/NavBar.jsx
+++ client/src/Components/NavBar.jsx
@@ -5,7 +5,7 @@ import linkedin from '../../dist/Images/linkedin.svg';
 import info from '../../dist/Images/info.svg';
 
 const NavBar = () => {
-  const navs = [
+  const texts = [
     {
       'link': '/',
       'where': 'Lorene'
@@ -24,7 +24,7 @@ const NavBar = () => {
     }
   ];
 
-  const buttons = [
+  const icons = [
     {
       'href': 'https://github.com/L-Chew',
       'src': github,
@@ -45,30 +45,30 @@ const NavBar = () => {
 
   return (
     <nav className='flex justify-between items-center text-sm py-4 px-1'>
-      {/* right text nav */}
+      {/* left text nav */}
       <ul className='flex rounded-lg'>
-        {navs.map((nav, index) => (
+        {texts.map((text, index) => (
           <div key={index} className='relative group'>
             <div className='absolute -inset-0.5 bg-gradient-to-r from-pink-600 to-purple-600 rounded-md blur opacity-0 group-hover:opacity-50 transition duration-1000 group-hover:duration-200'></div>
             <li className='relative py-1 px-2 hover:rounded-md transition duration-200 group-hover:scale-110'>
-              <Link to={nav.link}> {nav.where} </Link>
+              <Link to={text.link}> {text.where} </Link>
             </li>
           </div>
         ))}
       </ul>
-      {/* left icon nav */}
+      {/* right icon nav */}
       <ul className='flex rounded-lg'>
-        {buttons.map((button, index) => (
+        {icons.map((icon, index) => (
           <div key={index} className='relative group'>
             <div className='absolute -inset-0.5 bg-gradient-to-r from-pink-600 to-purple-600 rounded-md blur opacity-0 group-hover:opacity-50 transition duration-1000 group-hover:duration-200'></div>
             <li key={index} className='py-1 px-2'>
-              {button.href ? (
-                <a href={button.href} target='_blank' rel='noopener noreferrer'>
-                  <img className='h-9 transition duration-300 transform group-hover:scale-110' src={button.src} alt={button.alt}/>
+              {icon.href ? (
+                <a href={icon.href} target='_blank' rel='noopener noreferrer'>
+                  <img className='h-9 transition duration-300 transform group-hover:scale-110' src={icon.src} alt={icon.alt}/>
                 </a>
               ) : (
-                <Link to={button.link}>
-                  <img className='h-9 transition duration-300 transform group-hover:scale-110' src={button.src} alt={button.alt}/>
+                <Link to={icon.link}>
+                  <img className='h-9 transition duration-300 transform group-hover:scale-110' src={icon.src} alt={icon.alt}/>
                 </Link>
               )}
             </li>
