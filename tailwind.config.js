75105eeff98364a9725a63c31f1e9223a66483b2
Lorene Chew <lorene.chew@gmail.com>
2024-01-23 19:45:38 -0800

    tailwind.config, styles.css: added animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,11 +10,18 @@ module.exports = {
       },
       animation: {
         fadeIn: 'fadeIn 1s ease-in-out',
+        blob: 'blob 7s infinite',
       },
       keyframes: {
         fadeIn: {
           '0%': { opacity: 0 },
           '100%': { opacity: 1 },
+        },
+        blob: {
+          '0%': { transform: 'translate(0px, 0px) scale(1)' },
+          '33%': { transform: 'translate(30px, -50px) scale(1.1)' },
+          '66%': { transform: 'translate(-20px, 20px) scale(0.9)' },
+          '100%': { transform: 'translate(0px, 0px) scale(1)' },
         },
       },
     },
