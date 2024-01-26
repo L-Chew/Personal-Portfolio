845dbe1291d5d06d84e973e69741fe1e727edb77
Lorene Chew <lorene.chew@gmail.com>
2024-01-26 00:17:35 -0800

    Modal: added svgs for confirmation box and modified css

--- client/src/Components/Modal.jsx
+++ client/src/Components/Modal.jsx
@@ -1,9 +1,8 @@
 import React, { useState } from 'react';
-// import { createPortal } from 'react-dom';
 import mail from '../../dist/Images/mail.png';
 import closeButton from '../../dist/Images/closeButton.svg';
 import checkMark from '../../dist/Images/checkMark.svg';
-import doubleExclamationMark from '../../dist/Images/doubleExclamationMark.svg';
+import exclamationMark from '../../dist/Images/exclamationMark.svg';
 
 const Modal = ({ validity, open, onClose }) => {
   return validity ? (
@@ -11,17 +10,14 @@ const Modal = ({ validity, open, onClose }) => {
       className={`fixed inset-0 overflow-y-auto bg-[#121033] bg-opacity-80 ${open ? 'visible z-50 animate-fadeIn' : 'invisible'}`}
     >
       <div className='flex min-h-screen items-center'>
-        <div className='relative flex flex-col items-center mx-auto max-w-sm rounded-md bg-[#cbd5e1] p-6 shadow-lg md:max-w-md'>
-          <h3 className='text-lg font-black text-black'>
-            Email Sent Successfully
-          </h3>
-          <img className='w-1/2' src={checkMark} />
-          <p className='text-md text-black text-center'>
-          Thank you for your message.
-          <br></br>
-          I will get back to you soon!
+        <div className='relative mx-auto flex max-w-sm flex-col items-center gap-y-4 rounded-3xl bg-[#cbd5e1] p-6 py-12 shadow-lg md:max-w-md'>
+          <h3 className='text-3xl font-semibold text-[#3c3b67]'>Email Sent</h3>
+          <img className='w-1/3' src={checkMark} />
+          <p className='text-center text-xl text-[#3c3b67]'>
+            Thank you for your message.
+            <br></br>I will get back to you soon!
           </p>
-          <div className='mt-4 flex w-full justify-center text-black'>
+          <div className='mt-4 flex w-full justify-center'>
             <img
               className='w-10 rounded-3xl bg-slate-200 bg-opacity-0 mix-blend-multiply transition duration-1000 hover:bg-opacity-50 hover:duration-200'
               src={closeButton}
@@ -36,15 +32,10 @@ const Modal = ({ validity, open, onClose }) => {
       className={`fixed inset-0 overflow-y-auto bg-[#121033] bg-opacity-80 ${open ? 'visible z-50 animate-fadeIn' : 'invisible'}`}
     >
       <div className='flex min-h-screen items-center '>
-        <div className='relative flex flex-col items-center mx-auto w-full max-w-sm rounded-md bg-[#cbd5e1] p-6 shadow-lg md:max-w-md '>
-          <h3 className='text-lg font-black text-black'>
-            Email Unsuccessful!
-          </h3>
-          <img className='w-1/2' src={doubleExclamationMark} />
-          <p className='text-md text-black text-center'>
-            Please enter a valid email!
-          </p>
-          <div className='mt-4 flex w-full justify-center text-black text-center'>
+        <div className='relative mx-auto flex max-w-sm flex-col items-center gap-y-4 rounded-3xl bg-[#cbd5e1] p-6 py-16 shadow-lg md:max-w-md '>
+          <img className='w-1/3 pb-4' src={exclamationMark} />
+          <p className='text-center text-2xl text-[#3c3b67]'>Please enter a valid email!</p>
+          <div className='mt-4 flex w-full justify-center text-center text-black'>
             <img
               className='w-10 rounded-3xl bg-slate-200 bg-opacity-0 mix-blend-multiply transition duration-1000 hover:bg-opacity-50 hover:duration-200'
               src={closeButton}
