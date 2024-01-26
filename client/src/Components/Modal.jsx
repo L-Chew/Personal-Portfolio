049e3c9482a9fe9a20cbc02cd350676115b4b2e9
Lorene Chew <lorene.chew@gmail.com>
2024-01-25 23:26:27 -0800

    Modal: added separate modal layout for invalid form submission due to email failure

--- client/src/Components/Modal.jsx
+++ client/src/Components/Modal.jsx
@@ -1,24 +1,52 @@
 import React, { useState } from 'react';
-import { createPortal } from 'react-dom';
+// import { createPortal } from 'react-dom';
 import mail from '../../dist/Images/mail.png';
 import closeButton from '../../dist/Images/closeButton.svg';
+import checkMark from '../../dist/Images/checkMark.svg';
+import doubleExclamationMark from '../../dist/Images/doubleExclamationMark.svg';
 
-const Modal = ({ open, onClose }) => {
-  return (
+const Modal = ({ validity, open, onClose }) => {
+  return validity ? (
     <div
-      className={`fixed inset-0 overflow-y-auto bg-[#121033] bg-opacity-80 ${open ? 'animate-fadeIn z-50 visible' : 'invisible'}`}
+      className={`fixed inset-0 overflow-y-auto bg-[#121033] bg-opacity-80 ${open ? 'visible z-50 animate-fadeIn' : 'invisible'}`}
     >
-      <div className='flex min-h-screen items-center '>
-        <div className='relative mx-auto w-full max-w-md rounded-md bg-[#cbd5e1] p-6 shadow-lg'>
+      <div className='flex min-h-screen items-center'>
+        <div className='relative flex flex-col items-center mx-auto max-w-sm rounded-md bg-[#cbd5e1] p-6 shadow-lg md:max-w-md'>
           <h3 className='text-lg font-black text-black'>
             Email Sent Successfully
           </h3>
-          <p className='text-md text-black'>
-            Thank you for your message. I will get back to you soon!
+          <img className='w-1/2' src={checkMark} />
+          <p className='text-md text-black text-center'>
+          Thank you for your message.
+          <br></br>
+          I will get back to you soon!
           </p>
           <div className='mt-4 flex w-full justify-center text-black'>
             <img
-              className='w-10 bg-pink-300 bg-opacity-0 mix-blend-multiply transition duration-1000 hover:bg-opacity-50 hover:duration-200'
+              className='w-10 rounded-3xl bg-slate-200 bg-opacity-0 mix-blend-multiply transition duration-1000 hover:bg-opacity-50 hover:duration-200'
+              src={closeButton}
+              onClick={onClose}
+            ></img>
+          </div>
+        </div>
+      </div>
+    </div>
+  ) : (
+    <div
+      className={`fixed inset-0 overflow-y-auto bg-[#121033] bg-opacity-80 ${open ? 'visible z-50 animate-fadeIn' : 'invisible'}`}
+    >
+      <div className='flex min-h-screen items-center '>
+        <div className='relative flex flex-col items-center mx-auto w-full max-w-sm rounded-md bg-[#cbd5e1] p-6 shadow-lg md:max-w-md '>
+          <h3 className='text-lg font-black text-black'>
+            Email Unsuccessful!
+          </h3>
+          <img className='w-1/2' src={doubleExclamationMark} />
+          <p className='text-md text-black text-center'>
+            Please enter a valid email!
+          </p>
+          <div className='mt-4 flex w-full justify-center text-black text-center'>
+            <img
+              className='w-10 rounded-3xl bg-slate-200 bg-opacity-0 mix-blend-multiply transition duration-1000 hover:bg-opacity-50 hover:duration-200'
               src={closeButton}
               onClick={onClose}
             ></img>
