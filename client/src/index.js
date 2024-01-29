41384b62a12241ee065292658c7e92a5aae20aef
Lorene Chew <lorene.chew@gmail.com>
2024-01-29 03:56:22 -0800

    fixed capitalization in path to Components/App.jsx

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './styles.css';
-import App from './components/App.jsx';
+import App from './Components/App.jsx';
 import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('app'));
