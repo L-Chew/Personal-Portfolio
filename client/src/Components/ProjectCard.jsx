c38a184c519abe1711437edaa81edb4e40cebd9c
Lorene Chew <lorene.chew@gmail.com>
2024-01-28 17:07:35 -0800

    updated placeholder button logic; updated projects data

--- client/src/Components/ProjectCard.jsx
+++ client/src/Components/ProjectCard.jsx
@@ -18,15 +18,19 @@ const ProjectCard = ({ project }) => {
       <div className=' absolute inset-0 flex flex-col items-center justify-center gap-2 rounded-3xl px-4 pt-5 text-white opacity-0 group-hover:bg-white group-hover:bg-opacity-30 group-hover:opacity-100'>
         <h1 className='text-2xl xl:text-4xl'>{project.application}</h1>
         <p className=' text-lg xl:text-lg'>{project.description}</p>
-        <div className='p-6 pt-0'>
-          <button
-            onClick={handleClick}
-            className='cursor-pointerpx-3 rounded-3xl bg-[#8a51cb] text-center align-middle font-sans text-xs font-bold uppercase text-white shadow-md shadow-gray-900/10 transition-all hover:shadow-lg hover:shadow-gray-900/20 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none md:py-3 md:px-6 disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none px-3 py-1.5'
-            type='button'
-          >
-            View Project
-          </button>
-        </div>
+        {project.application !== 'Coming Soon' ?
+          <div className='p-6 pt-0'>
+            <button
+              onClick={handleClick}
+              className='cursor-pointerpx-3 rounded-3xl bg-[#8a51cb] text-center align-middle font-sans text-xs font-bold uppercase text-white shadow-md shadow-gray-900/10 transition-all hover:shadow-lg hover:shadow-gray-900/20 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none md:py-3 md:px-6 disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none px-3 py-1.5'
+              type='button'
+            >
+              View Project
+            </button>
+          </div>
+          :
+          ''
+        }
       </div>
     </div>
   );
--- client/src/Components/Projects.jsx
+++ client/src/Components/Projects.jsx
@@ -2,6 +2,10 @@ import React from 'react';
 import pupper from '../../dist/Images/Projects/pupper.png';
 import catwalk from '../../dist/Images/Projects/catwalk.png';
 import products from '../../dist/Images/Projects/products.png';
+import placeholder1 from '../../dist/Images/Projects/placeholder1.jpeg';
+import placeholder2 from '../../dist/Images/Projects/placeholder2.jpeg';
+import placeholder3 from '../../dist/Images/Projects/placeholder3.jpeg';
+import placeholder5 from '../../dist/Images/Projects/placeholder5.jpeg'
 import ProjectCard from './ProjectCard.jsx';
 
 const Projects = () => {
@@ -17,16 +21,6 @@ const Projects = () => {
       techStack: ['React', 'Javascript', 'MongoDB'],
     },
     {
-      key: 2,
-      year: 'MAR 2022  -  MAR 2022',
-      application: 'Atelier API',
-      description:
-        'A microservice architecture RESTful API; replace existing monoliithic application',
-      img: products,
-      link: 'https://github.com/Team-Luigi/Products/tree/main',
-      techStack: ['React', 'Javascript', 'SQL', 'Material UI'],
-    },
-    {
       key: 3,
       year: 'FEB 2022  -  FEB 2022',
       application: 'Catwalk',
@@ -38,43 +32,40 @@ const Projects = () => {
     },
     {
       key: 4,
-      year: 'MAR 2022  -  APR 2022',
-      application: 'Pupper',
-      description:
-        'Responsive, mobile-frst web application where clients match with other puppies by swiping based on first impression. When matched, you enter into your own chat room to further interact and coordinate playdates.',
-      img: pupper,
-      link: 'https://github.com/projectPupper/pupper',
-      techStack: ['React', 'Javascript', 'MongoDB'],
+      year: '',
+      application: 'Coming Soon',
+      description: '',
+      img: placeholder1,
+      link: '',
+      techStack: [],
     },
     {
       key: 5,
-      year: 'FEB 2022  -  FEB 2022',
-      application: 'Catwalk',
+      year: '',
+      application: 'Coming Soon',
+      description: '',
+      img: placeholder2,
+      link: '',
+      techStack: [],
+    },
+    {
+      key: 2,
+      year: 'MAR 2022  -  MAR 2022',
+      application: 'Atelier API',
       description:
-        'A user interface for the Q&A widget of an e-commerce web application',
-      img: catwalk,
-      link: 'https://github.com/RFP58-Clerics/Project-Catwalk',
-      techStack: ['React', 'Javascript', 'POSTgresSQL', 'CSS'],
+        'A microservice architecture RESTful API; replace existing monoliithic application',
+      img: products,
+      link: 'https://github.com/Team-Luigi/Products/tree/main',
+      techStack: ['React', 'Javascript', 'SQL', 'Material UI'],
     },
     {
       key: 6,
-      year: 'FEB 2022  -  FEB 2022',
-      application: 'Catwalk',
-      description:
-        'A user interface for the Q&A widget of an e-commerce web application',
-      img: catwalk,
-      link: 'https://github.com/RFP58-Clerics/Project-Catwalk',
-      techStack: ['React', 'Javascript', 'POSTgresSQL', 'CSS'],
-    },
-    {
-      key: 7,
-      year: 'MAR 2022  -  APR 2022',
-      application: 'Pupper',
-      description:
-        'Responsive, mobile-frst web application where clients match with other puppies by swiping based on first impression. When matched, you enter into your own chat room to further interact and coordinate playdates.',
-      img: pupper,
-      link: 'https://github.com/projectPupper/pupper',
-      techStack: ['React', 'Javascript', 'MongoDB'],
+      year: '',
+      application: 'Coming Soon',
+      description: '',
+      img: placeholder3,
+      link: '',
+      techStack: [],
     }
   ];
 
