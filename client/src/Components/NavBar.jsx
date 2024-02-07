d90522bec4c0f5d8bba5283f6e6a8010946d2856
Lorene Chew <lorene.chew@gmail.com>
2024-02-07 15:38:21 -0800

    right icon flex-col on smaller screens

--- client/src/Components/NavBar.jsx
+++ client/src/Components/NavBar.jsx
@@ -1,10 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import github from '../../dist/Images/Navigation/github.svg';
 import linkedin from '../../dist/Images/Navigation/linkedin.svg';
 import info from '../../dist/Images/Navigation/info.svg';
+import menu from '../../dist/Images/Navigation/menu.svg';
 
 const NavBar = () => {
+  const [showMenu, setShowMenu] = useState(false);
+
   const texts = [
     {
       link: '/',
@@ -43,46 +46,51 @@ const NavBar = () => {
     },
   ];
 
+  const handleClick = () => {
+    setShowMenu(!showMenu);
+  }
+
   return (
     <nav className='flex items-center justify-between bg-[#0f172a] bg-opacity-80 px-1 py-4 text-sm'>
       {/* left text nav */}
-      <ul className='flex rounded-lg'>
-        {texts.map((text, index) => (
-          <div key={index} className='group relative'>
-            <div className='absolute -inset-0.5 rounded-md bg-gradient-to-r from-pink-600 to-purple-600 opacity-0 blur transition duration-1000 group-hover:opacity-50 group-hover:duration-200'></div>
-            <li className='relative px-2 py-1 transition duration-200 hover:rounded-md group-hover:scale-110'>
-              <Link to={text.link}> {text.where} </Link>
-            </li>
-          </div>
-        ))}
-      </ul>
+        <ul className='flex rounded-lg'>
+          {texts.map((text, index) => (
+            <div key={index} className='group relative'>
+              <div className='absolute -inset-0.5 rounded-md bg-gradient-to-r from-pink-600 to-purple-600 opacity-0 blur transition duration-1000 group-hover:opacity-50 group-hover:duration-200'></div>
+              <li className='relative px-2 py-1 transition duration-200 hover:rounded-md group-hover:scale-110'>
+                <Link to={text.link}> {text.where} </Link>
+              </li>
+            </div>
+          ))}
+        </ul>
       {/* right icon nav */}
-      <ul className='flex rounded-lg'>
-        {icons.map((icon, index) => (
-          <div key={index} className='group relative'>
-            <div className='absolute -inset-0.5 rounded-md bg-gradient-to-r from-pink-600 to-purple-600 opacity-0 blur transition duration-1000 group-hover:opacity-50 group-hover:duration-200'></div>
-            <li key={index} className='px-2 py-1'>
-              {icon.href ? (
-                <a href={icon.href} target='_blank' rel='noopener noreferrer'>
-                  <img
-                    className='h-9 transform transition duration-300 group-hover:scale-110'
-                    src={icon.src}
-                    alt={icon.alt}
-                  />
-                </a>
-              ) : (
-                <Link to={icon.link}>
-                  <img
-                    className='h-9 transform transition duration-300 group-hover:scale-110'
-                    src={icon.src}
-                    alt={icon.alt}
-                  />
-                </Link>
-              )}
-            </li>
-          </div>
-        ))}
-      </ul>
+        <ul className='rounded-lg flex md:flex-row flex-col'>
+          {icons.map((icon, index) => (
+            <div key={index} className='group relative'>
+              <div className='absolute -inset-0.5 rounded-md bg-gradient-to-r from-pink-600 to-purple-600 opacity-0 blur transition duration-1000 group-hover:opacity-50 group-hover:duration-200'></div>
+              <li key={index} className='px-2 py-1'>
+                {icon.href ? (
+                  <a href={icon.href} target='_blank' rel='noopener noreferrer'>
+                    <img
+                      className='h-9 transform transition duration-300 group-hover:scale-110'
+                      src={icon.src}
+                      alt={icon.alt}
+                    />
+                  </a>
+                ) : (
+                  <Link to={icon.link}>
+                    <img
+                      className='h-9 transform transition duration-300 group-hover:scale-110'
+                      src={icon.src}
+                      alt={icon.alt}
+                    />
+                  </Link>
+                )}
+              </li>
+            </div>
+          ))}
+        </ul>
+
     </nav>
   );
 };
